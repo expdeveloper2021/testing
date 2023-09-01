147a98d4b858105f99d0f721e966905c1a80bffc
expdeveloper2021 <fahimraza089@gmail.com>
2023-09-01 22:21:55 +0500

    Some more questions added

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
 import inquirer from "inquirer";
 
-let answers = await inquirer.prompt({
-    message: "Have you ever felt in love?",
-    type: "input",
-    name: "userResponse"
-})
+let answers = await inquirer.prompt(
+    [
+        {
+            message: "What is your name?",
+            type: "input",
+            name: "userResponse"
+        },
+        {
+            message: "What was your age that time?",
+            type: "number",
+            name: "userAge"
+        },
+    ]
+)
 
 console.log(answers.userResponse)
